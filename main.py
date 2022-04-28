13b5563f6dddd1e4d7a52f02d4a40eb82b857b35
914 <mralek2017@gmail.com>
2022-04-28 12:14:38 +0500

    работает

--- main.py
+++ main.py
@@ -4,24 +4,37 @@ from tkinter import *
 
 
 
-def get_html(self, url):
+class MyWindow:
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry('400x400')
+        self.btn = Button(width=15, height=2, text='Показать курс', command=self.main)
+        self.btn.place(relx=.5, rely=.4, anchor='c')
+        self.txt = Entry(width=25)
+        self.txt.place(relx=.5, rely=.2, anchor='c')
+        self.lbl = Label(root, text='Курс Compound', font=('Times New Roman', 16))
+        self.lbl.place(relx=.5, rely=.07, anchor='center')
+
+
+    def get_html(self, url):
         html_content = requests.get(url).text
         return html_content
 
-def get_data(self, html):
-    soup = BeautifulSoup(html, 'lxml')
-    # t = soup.find('div', class_ = "temp fact__temp fact__temp_size_s").find('span', class_ = "temp__value temp__value_with-unit")
-    t = soup.find('div', {'class': "sc-16r8icm-0 kjciSH priceTitle"}).find('span')
-    return t.text
+    def get_data(self, html):
+        soup = BeautifulSoup(html, 'lxml')
+        # t = soup.find('div', class_ = "temp fact__temp fact__temp_size_s").find('span', class_ = "temp__value temp__value_with-unit")
+        t = soup.find('div', {'class': "sc-16r8icm-0 kjciSH priceTitle"}).find('span')
+        return t.text
 
-def main(self):
-    self.txt.delete(0, END)
-    url = 'https://coinmarketcap.com/currencies/compound/'
-    res = self.get_html(url)
-    self.txt.insert(0, self.get_data(res))
+    def main(self):
+        self.txt.delete(0, END)
+        url = 'https://coinmarketcap.com/currencies/compound/'
+        res = self.get_html(url)
+        self.txt.insert(0, self.get_data(res))
 
 
 if __name__ == '__main__':
     root = Tk()
     win = MyWindow(root)
-    root.mainloop()
+
+root.mainloop()
